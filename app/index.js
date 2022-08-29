75d9ef44f6437c9360bf84edeb50471b8e79b5f3
Gary Rutland <me@garyrutland.com>
2022-08-29 17:10:42 +0100

    Updated to include version and github info

--- app/index.js
+++ app/index.js
@@ -1,80 +1,79 @@
-const fs = require('fs');
-const express = require('express');
-const YAML = require('yaml');
-const {graphql} = require("@octokit/graphql");
+const fs = require('fs')
+const express = require('express')
+const YAML = require('yaml')
+const {graphql} = require('@octokit/graphql')
+const harmony = require('./harmony.json')
 
-const app = express();
-const port = 80;
+const app = express()
+const port = 80
 
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
+
+app.get('/module/catalog/harmony.json', async (req, res) => {
+    res.header('Content-Type', 'application/json')
+    res.send(JSON.stringify(harmony))
+})
 
 app.get('/module/catalog', async (req, res) => {
-    const config = getConfig();
+    const config = getConfig()
     const graphqlWithAuth = graphql.defaults({
         headers: {
             authorization: `token ${config.github.token}`,
         },
-    });
+    })
 
     const {search} = await graphqlWithAuth(`
-{
-  search(query: "org:${config.github.org} sort:name-asc", type: REPOSITORY, first: 10) {
-     repositoryCount
-    nodes {
-      ... on Repository {
-        name
-        nameWithOwner
-        url
-        description
-        visibility
-        forkCount
-        stargazerCount
-        issues {
-          totalCount
-        }
-        pullRequests {
-          totalCount
-        }
-        languages(first: 10) {
-          nodes {
-            name
-          }
-        }
-        repositoryTopics(first: 10) {
-          nodes {
-            topic {
-              name
+        {
+            search(query: "org:${config.github.org} sort:name-asc", type: REPOSITORY, first: 10) {
+                repositoryCount
+                nodes {
+                    ... on Repository {
+                        name
+                        nameWithOwner
+                        url
+                        description
+                        visibility
+                        forkCount
+                        stargazerCount
+                        issues {
+                            totalCount
+                        }
+                        pullRequests {
+                            totalCount
+                        }
+                        languages(first: 10) {
+                            nodes {
+                                name
+                            }
+                        }
+                        repositoryTopics(first: 10) {
+                            nodes {
+                                topic {
+                                    name
+                                }
+                            }
+                        }
+                    }
+                }
             }
-          }
         }
-      }
-    }
-  }
-}
-`);
+`)
 
-    res.render('index', {count: search.repositoryCount, repos: search.nodes});
+    res.render('index', {count: search.repositoryCount, repos: search.nodes})
 })
-
-app.get('/module/catalog/module.json', async (req, res) => {
-    res.send({
-        name: "Catalog",
-        description: "Harmony catalog module.",
-    });
-});
 
 
 app.listen(port, () => {
-    console.log(`Catalog app listening on port ${port}`);
+    console.log(`Catalog app listening on port ${port}`)
 })
 
 function getConfig() {
-    const regex = /\${{[A-Z0-9_]+}}/g;
+    const regex = /\${{[A-Z0-9_]+}}/g
 
-    let file = fs.readFileSync('/root/.harmony/config/main.yaml', 'utf8');
+    let file = fs.readFileSync('/root/.harmony/config/main.yaml', 'utf8')
     for (const match of file.match(regex)) {
         file = file.replace(match, process.env[match.slice(3, -2)] || null)
     }
 
-    return YAML.parse(file);
+    return YAML.parse(file)
 }
